fe0a8d7c6cf01fabd5f12a8d345524fe972458d3
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 14:41:01 -0400

    only pass data vals

--- jobs/deploy.groovy
+++ jobs/deploy.groovy
@@ -5,7 +5,7 @@ println "building deploy job..."
 def jfile = readFileFromWorkspace "pipelines/deploy.groovy"
 def vars = binding.getVariables()
 
-def fixParams(data) {
+def fixData(data) {
     println "---- function fixParams -----"
     println data.getClass()
     if (data instanceof Collection) {
@@ -13,10 +13,8 @@ def fixParams(data) {
         // fixParams(i.value)
     }
 }
-for (i in vars) {
-    println "${i.key} : ${i.value}"
-}
-fixParams(vars)
+
+fixData(vars['data'])
 // for (i in vars) {
 //     if (i.value instanceof Collection) {
 //         i.value = i.value.collect{ "'$it'" }
