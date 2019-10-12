2e52140b17cf793f3c933aa077b659d3ef5c689e
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 14:32:47 -0400

    try iteration

--- jobs/deploy.groovy
+++ jobs/deploy.groovy
@@ -5,12 +5,29 @@ println "building deploy job..."
 def jfile = readFileFromWorkspace "pipelines/deploy.groovy"
 def vars = binding.getVariables()
 
+def fixParams(data) {
+    for (i in data) {
+        if (i.value instanceof Collection) {
+            println "Iterating through ${i}..."
+            fixParams(i.value)
+        }
+        else {
+            i.value = "'$it'"
+        }
+    }
+}
+for (i in vars) {
+    println "${i.key} : ${i.value}"
+}
+fixParams(vars)
 // for (i in vars) {
 //     if (i.value instanceof Collection) {
 //         i.value = i.value.collect{ "'$it'" }
 //     }
 // }
-
+for (i in vars) {
+    println "${i.key} : ${i.value}"
+}
 
 
 def engine = new SimpleTemplateEngine()
