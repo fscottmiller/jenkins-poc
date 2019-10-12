90aa8abfd4e4b5a582d349591b965c7313634e21
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 15:44:00 -0400

    try

--- scripts/get2.groovy
+++ scripts/get2.groovy
@@ -16,7 +16,7 @@ def fixData(data) {
 def getPipelineScript(String file, Map vars) {
     println "----- function getPipelineScript -----"
     fixData(vars)
-    println "a"
+    println file
     def script = new SimpleTemplateEngine().createTemplate(file).make(vars).toString()
     println "b"
     return script
