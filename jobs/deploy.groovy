5b51f6dced0dcdbbc919d878ebd57b475349046c
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 15:25:53 -0400

    try using class

--- jobs/deploy.groovy
+++ jobs/deploy.groovy
@@ -2,7 +2,7 @@ evaluate(readFileFromWorkspace("scripts/getPipelineScript.groovy"))
 
 println "in deploy.groovy"
 
-script = getPipelineScript("pipelines/deploy.groovy", binding.getVariables()['data'])
+script = Utils.getPipelineScript("pipelines/deploy.groovy", binding.getVariables()['data'])
 
 pipelineJob("${project}-Deploy") {
     displayName('Deploy')
--- scripts/getPipelineScript.groovy
+++ scripts/getPipelineScript.groovy
@@ -1,26 +1,23 @@
 import groovy.text.SimpleTemplateEngine
 
-println "fixData"
-def fixData(data) {
-    println "---- function fixParams -----"
-    for (i in data) {
-        if (i.value instanceof Map) {
-            println "Iterating ${i.key}..."
-            fixData(i.value)
-        } else if (i.value instanceof Collection) {
-            println "Fixing collection values"
-            i.value = i.value.collect{ "'$it'" }
+Class Utils {
+    static void fixData(data) {
+        println "---- function fixParams -----"
+        for (i in data) {
+            if (i.value instanceof Map) {
+                println "Iterating ${i.key}..."
+                fixData(i.value)
+            } else if (i.value instanceof Collection) {
+                println "Fixing collection values"
+                i.value = i.value.collect{ "'$it'" }
+            }
         }
     }
-}
 
-println "getPipelineScript"
-def getPipelineScript(String file, LinkedHashMap vars) {
-    println "Workspace: ${WORKSPACE}"
-    def jfile = readFileFromWorkspace "pipelines/deploy.groovy"
-    fixData(vars)
-    def script = new SimpleTemplateEngine().createTemplate(jfile).make(vars).toString()
-    return script
+    static String getPipelineScript(String file, Map vars) {
+        def jfile = readFileFromWorkspace file
+        fixData(vars)
+        def script = new SimpleTemplateEngine().createTemplate(jfile).make(vars).toString()
+        return script
+    }
 }
-
-println binding.getVariables()
