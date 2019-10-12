83849f5f3411c73f257f6d48cc11ccd815b68d58
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 14:56:58 -0400

    try abstracting script fns

--- jobs/deploy.groovy
+++ jobs/deploy.groovy
@@ -1,43 +1,13 @@
-import groovy.text.SimpleTemplateEngine
+evaluate(new File("../scripts/getPipelineScript.groovy"))
 
-println "building deploy job..."
+script = getPipelineScript("pipelines/deploy.groovy", binding.getVariables()['data'])
 
-def jfile = readFileFromWorkspace "pipelines/deploy.groovy"
-def vars = binding.getVariables()
-
-def fixData(data) {
-    println "---- function fixParams -----"
-    for (i in data) {
-        if (i.value instanceof Map) {
-            println "Iterating ${i.key}..."
-            fixData(i.value)
-        } else if (i.value instanceof Collection) {
-            println "Fixing collection values"
-            i.value = i.value.collect{ "'$it'" }
-        }
-    }
-}
-
-fixData(vars['data'])
-// for (i in vars) {
-//     if (i.value instanceof Collection) {
-//         i.value = i.value.collect{ "'$it'" }
-//     }
-// }
-println "------------------------------------------------------------------------------"
-for (i in vars) {
-    println "${i.key} : ${i.value}"
-}
-
-
-def engine = new SimpleTemplateEngine()
-template = engine.createTemplate(jfile).make(vars).toString()
 
 pipelineJob("${project}-Deploy") {
     displayName('Deploy')
     definition {
         cps { 
-            script(template)
+            script(script)
         }
     }
 }
---
+++ scripts/getPipelineScript.groovy
@@ -0,0 +1,21 @@
+import groovy.text.SimpleTemplateEngine
+
+def fixData(data) {
+    println "---- function fixParams -----"
+    for (i in data) {
+        if (i.value instanceof Map) {
+            println "Iterating ${i.key}..."
+            fixData(i.value)
+        } else if (i.value instanceof Collection) {
+            println "Fixing collection values"
+            i.value = i.value.collect{ "'$it'" }
+        }
+    }
+}
+
+def getPipelineScript(String file, Map vars) {
+    def jfile = readFileFromWorkspace "pipelines/deploy.groovy"
+    fixData(vars)
+    def script = new SimpleTemplateEngine().createTemplate(jfile).make(vars).toString()
+    return script
+}
