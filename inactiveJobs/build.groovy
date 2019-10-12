d8bab47c497b2d2342e2dc33776b9c510af60e13
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 15:00:27 -0400

    delete unused jobs; debug

--- inactiveJobs/build.groovy
+++ inactiveJobs/build.groovy
@@ -1,11 +1,28 @@
-job("${project}-Build") {
+import groovy.text.SimpleTemplateEngine
+
+println "building build job..."
+
+def jfile = readFileFromWorkspace "pipelines/build.groovy"
+def vars = binding.getVariables()
+
+// for (i in vars) {
+//     if (i.value instanceof Collection) {
+//         i.value = i.value.collect{ "'$it'" }
+//     }
+// }
+
+for (i in vars) {
+    println "${i.key} : ${i.value}"
+}
+
+def engine = new SimpleTemplateEngine()
+template = engine.createTemplate(jfile).make(vars).toString()
+
+pipelineJob("${project}-Build") {
     displayName('Build')
-    description("Build your project and push it to artifactory.")
-    parameters {
-        activeChoiceParam("Branch") {
-        groovyScript {
-            script("return (\"git ls-remote --heads ${repository}\").execute().text.readLines().collect { it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '') }")
-            }
+    definition {
+        cps { 
+            script(template)
         }
     }
 }
--- inactiveJobs/manual.groovy
+++
@@ -1,7 +0,0 @@
-job("${project}-Manual") {
-    displayName('Manual Tests')
-    description("Check the results of your manual testing..")
-    parameters {
-        choiceParam("Environment", environments)
-    }
-}
