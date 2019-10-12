1cf159b42f9a7facaecf2f02b4ccb36bc01ec7bd
Scott Miller <fsm5yd@virginia.edu>
2019-10-12 15:03:55 -0400

    debug

--- jobs/deploy.groovy
+++ jobs/deploy.groovy
@@ -1,5 +1,7 @@
 evaluate(readFileFromWorkspace("scripts/getPipelineScript.groovy"))
 
+evaluate("println 'foo'")
+
 println "in deploy.groovy"
 
 script = getPipelineScript("pipelines/deploy.groovy", binding.getVariables()['data'])
